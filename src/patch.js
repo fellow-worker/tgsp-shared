43157ab4a2dc9fdd64543115560871653a8e6965
Fellow Worker <fellow-worker@outlook.com>
2020-04-10 11:18:38 +0000

    temp

--- src/patch.js
+++ src/patch.js
@@ -19,3 +19,23 @@ export const patch = (source, patch, excluded) => {
     // return the result
     return source;
 }
+
+/**
+ * This method will limit the properties in patch given source
+ * @param {Object} source The source object that contains the describtion
+ * @param {Object} patch The patch object which contains the new values
+ * @param {Array} excluded A list of properties to exclude in patch
+ * @return {Object} the patch object stripped from it's field that are excluded or not set
+ */
+export const limit = (source,patch, excluded) => {
+    // Ensure that excluded is an array 
+    if(Array.isArray(excluded) === false) excluded = [];
+    const sourceKeys = Object.keys(source);
+
+    Object.keys(patch).forEach(key => {
+        if(sourceKeys.includes(key) === false) delete(patch[key]);
+        if(excluded.includes(key) === true) delete(patch[key]);
+    });
+
+    return patch;
+}
--- tests/patch.js
+++ tests/patch.js
@@ -41,7 +41,7 @@ describe('Patch Tests', () => {
         chai.expect(result.goal).to.not.exist;
     });   
 
-    it("patch - exclded", async () => {
+    it("patch - excluded", async () => {
 
         const source = { name : "a", goal : 1 };
         const patch = { name : "b", goal : 2 };
@@ -50,4 +50,54 @@ describe('Patch Tests', () => {
         chai.expect(result.name).to.equal(patch.name);
         chai.expect(result.goal).to.equal(source.goal);
     });   
+
+    it("limit - simple test", async () => {
+
+        const source = { name : "a" };
+        const patch = { name : "b" };
+
+        const result = patcher.limit(source, patch);
+        chai.expect(result.name).to.equal(patch.name);
+    });
+
+    it("limit - souce multi prop", async () => {
+
+        const source = { name : "a", goal : 1 };
+        const patch = { name : "b" };
+
+        const result = patcher.limit(source, patch);
+        chai.expect(result.name).to.equal(patch.name);
+        chai.expect(result.goal).to.not.exist;
+    });  
+
+    it("limit - multi prop", async () => {
+
+        const source = { name : "a", goal : 1 };
+        const patch = { name : "b", goal : 2 };
+
+        const result = patcher.limit(source, patch);
+        chai.expect(result.name).to.equal(patch.name);
+        chai.expect(result.goal).to.equal(patch.goal);
+    });   
+
+    it("limit - target multi prop", async () => {
+
+        const source = { name : "a" };
+        const patch = { name : "b", goal : 2 };
+
+        const result = patcher.limit(source, patch);
+        chai.expect(result.name).to.equal(patch.name);
+        chai.expect(result.goal).to.not.exist;
+    });   
+
+    it("patch - excluded", async () => {
+
+        const source = { name : "a", goal : 1 };
+        const patch = { name : "b", goal : 2 };
+
+        const result = patcher.limit(source, patch, ["goal"]);
+        chai.expect(result.name).to.equal(patch.name);
+        chai.expect(result.goal).to.not.exist;
+    });   
+
 });
